878df88ed84f46d1bf0d04c4a8c8da60204bdd6d
Antoine P <antoine.plaineau@etudiant.univ-lr.fr>
2022-10-29 01:23:59 +0200

    Issue - close modal + fix style

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelectorAll(".close");
+
+// Functions
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Close modal event
+modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
+
+
+
